ae8a7844163cffe31f2905d7b541cbeea517c1e2
Enzo Lahana <enzo.lahana@artfx.fr>
2026-01-19 14:13:45 +0100

    [Add] .bat

---
+++ FatePipe.bat
@@ -0,0 +1,1 @@
+rez env FatePipe pyside6 -- python -m "abspipeline.ui.browser"
---
+++ package.py
@@ -0,0 +1,8 @@
+name = "FatePipe"
+version = "0.1.0"
+requires = [
+    'qtpy',
+]
+
+def commands():
+    env.PYTHONPATH.append('{root}/python')
